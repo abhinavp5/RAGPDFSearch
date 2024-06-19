d96d3b482b52ce7593c55689b1c661c0c93d8c4c
Abhinav_p5 <abhinavpappu05@gmail.com>
2024-06-19 18:02:05 -0400

    got input from streamlit

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.retrievers import BM25Retriever
+import base64
+import streamlit as st
+from annotated_text import annotated_text
 import os
+
 load_dotenv()
 
 google_key = os.getenv("GOOGLE_API_KEY")
 pinecone_api_key = os.getenv("PINECONE_API_KEY")
 
+
+#performs the Semantic Search
 def semanticSearch(text, query):
     # llm = ChatGoogleGenerativeAI(model = "gemini-pro",temperature =0.7)
     # result = llm.invoke("What is a LLM?")
@@ -59,21 +65,72 @@ def semanticSearch(text, query):
 
     
 
+#used to extract text from pdf in a formate that can be formatte to Vector Stores
 def extractPDF(path):
     reader = PdfReader(path)
     num_pages = len(reader.pages)
+    
     text = ""
     for i in range(num_pages):
         text += reader.pages[i].extract_text()
     return text
 
+
+#running the web page in streamlit
+def runPage():
+    st.set_page_config(
+        page_title = "Magic Cntrl+f"
+    )
+    st.title("Magic Ctrl+F")
+    pdf_file = st.file_uploader("Upload a PDF file", type = "pdf")
+    search_query = st.text_input("Search Query")
+
+
+    pdf_content = "" ##the text from the pdf 
+    matches = []
+    input_file = ""
+    if pdf_file is not None:
+        st.write(displayPDF(pdf_file))
+
+        
+
+    if search_query and pdf_file is not None:
+        matches = semanticSearch(extractPDF(pdf_file), search_query)
+        results = [match for match in matches]
+        return results 
+    
+
+#used to extract the text from the pdf to dispaly in streamlit
+def displayPDF(file):
+    pdf_reader = PdfReader(file)
+    content = ""
+    for page in range(pdf_reader.get_num_pages()):
+        content+= pdf_reader.get_page(page).extract_text()
+    return content
+
+
+def highlightText(textBlock):
+    text = "Hello my name is Bob"
+    annotated_text(
+    (text,"", "#8cff66")
+)
+        
+
+
+
+
+
 def main():
-    path = "/Users/abhinavpappu/Documents/PersonalProjects/PDFSearcher/Lancet_20240618/Japan--health-after-the-earthquake_lancet.pdf"
-    text = extractPDF(path)
-    query = "how many people died"
-    matches = semanticSearch(text, query)
-    results = [match for match in matches]
-    print(results)
+    matches = runPage()
+    st.write(matches)
+
+    # path = "/Users/abhinavpappu/Documents/PersonalProjects/PDFSearcher/Lancet_20240618/Japan--health-after-the-earthquake_lancet.pdf"
+    # text = extractPDF(path)
+    # query = "how many people died"
+    # matches = semanticSearch(text, query)
+    # results = [match for match in matches]
+    # print(results)
 
 if __name__ =='__main__':
     main()
+
